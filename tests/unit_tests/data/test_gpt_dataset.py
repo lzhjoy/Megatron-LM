b12b0da49beda72f6362fff1c5c1829991896316
O2iginal <O2iginal@163.com>
2025-09-23 16:28:16 +0000

    test_gpt_dataset print

--- tests/unit_tests/data/test_gpt_dataset.py
+++ tests/unit_tests/data/test_gpt_dataset.py
@@ -4,15 +4,24 @@
 # Compile megatron.core.datasets.helpers_cpp dependencies before BlendedDataset import
 ##
 
+import os
 import random
+import sys
+
+os.environ["LOCAL_RANK"] = "0"
+os.environ["WORLD_SIZE"] = "1"
 
 import numpy
 import pytest
 import torch
 
-from megatron.core.datasets.blended_megatron_dataset_builder import BlendedMegatronDatasetBuilder
-from megatron.core.datasets.gpt_dataset import GPTDatasetConfig, MockGPTDataset
-from megatron.core.datasets.utils import compile_helpers
+sys.path.append("/data/pretrain-linear-moe/YuLan-Pretrain")
+
+from megatron.core.datasets.blended_megatron_dataset_builder import \
+    BlendedMegatronDatasetBuilder
+from megatron.core.datasets.gpt_dataset import (GPTDataset, GPTDatasetConfig,
+                                                MockGPTDataset)
+from megatron.core.datasets.utils import compile_helpers, get_blend_from_list
 from megatron.training.tokenizer.tokenizer import _NullTokenizer
 from tests.unit_tests.test_utilities import Utils
 
@@ -50,9 +59,8 @@ def test_mock_gpt_dataset():
         mid_level_dataset_surplus=0.005,
     )
 
-    datasets = BlendedMegatronDatasetBuilder(
-        MockGPTDataset, [100, 100, 100], lambda: True, config
-    ).build()
+    datasets = BlendedMegatronDatasetBuilder(MockGPTDataset, [100, 100, 100],
+                                             lambda: True, config).build()
 
     N = 10
 
@@ -85,12 +93,12 @@ def test_mock_gpt_dataset():
         mid_level_dataset_surplus=0.005,
     )
 
-    datasets = BlendedMegatronDatasetBuilder(
-        MockGPTDataset, [0, None, 0], lambda: True, config
-    ).build()
+    datasets = BlendedMegatronDatasetBuilder(MockGPTDataset, [0, None, 0],
+                                             lambda: True, config).build()
 
     sample = datasets[1][datasets[1].shuffle_index.argmax()]
-    argmax = sample['labels'].shape[0] - torch.flip(sample['labels'], [0]).argmax() - 1
+    argmax = sample['labels'].shape[0] - torch.flip(sample['labels'],
+                                                    [0]).argmax() - 1
 
     # Test add_extra_token_to_sequence
     assert sample['tokens'][argmax] != tokenizer.eod
@@ -98,12 +106,9 @@ def test_mock_gpt_dataset():
 
     # Test eod_mask_loss, drop_last_partial_validation_sequence
     assert argmax < sample['labels'].shape[0] - 1
-    assert torch.all(sample['labels'][argmax + 1 :] == 0)
-    assert not torch.any(
-        sample['loss_mask'][
-            torch.logical_and(sample['labels'] == tokenizer.eod, sample['labels'] == 0)
-        ]
-    )
+    assert torch.all(sample['labels'][argmax + 1:] == 0)
+    assert not torch.any(sample['loss_mask'][torch.logical_and(
+        sample['labels'] == tokenizer.eod, sample['labels'] == 0)])
 
     sample = datasets[1][None]
 
@@ -111,5 +116,81 @@ def test_mock_gpt_dataset():
     assert not torch.any(sample['loss_mask'])
 
 
+def test_gpt_dataset_with_loss_mask():
+    # if torch.distributed.is_available():
+    #     Utils.initialize_distributed()
+    #     if torch.distributed.get_rank() == 0:
+    #         compile_helpers()
+    #     torch.distributed.barrier()
+    # else:
+    #     compile_helpers()
+
+    data_path = []
+    for i in range(16):
+        d = f"/data/pretrain-linear-moe-dev/cache/datasets/IvanHU/aabcfab61cae2f6879866e11565c09b0a397c4690ee346b3135cd60c22f9d390/worker_{i}_input_ids_tmp"
+        if os.path.getsize(d + ".bin") > 0:
+            data_path.append("1")
+            data_path.append(d)
+
+    tokenizer = _NullTokenizer(vocab_size=_MOCK_VOCAB_SIZE)
+
+    config = GPTDatasetConfig(
+        random_seed=1234,
+        sequence_length=1024,
+        blend=get_blend_from_list(data_path),
+        split="100,0,0",
+        reset_position_ids=True,
+        reset_attention_mask=True,
+        eod_mask_loss=True,
+        tokenizer=tokenizer,
+        mid_level_dataset_surplus=0.005,
+    )
+
+    dataset, val_dataset, test_dataset = BlendedMegatronDatasetBuilder(
+        GPTDataset, [100, 100, 100], lambda: True, config).build()
+    print(type(dataset))
+
+    batch = dataset[0]
+    tokens = batch["tokens"].numpy()
+    labels = batch["labels"].numpy()
+    loss_mask = batch["loss_mask"].numpy()
+    attention_mask = batch["attention_mask"].numpy()
+    position_ids = batch["position_ids"].numpy()
+    print(tokens, tokens.shape)
+    print(labels, labels.shape)
+    print(loss_mask, loss_mask.shape)
+    print(attention_mask, attention_mask.shape)
+    print(position_ids, position_ids.shape)
+    # print(batch["dropout_mask"].numpy())
+
+    from transformers import AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained(
+        "/data/pretrain-linear-moe/preprocess/modify_tokenizer/YuLan-Mini-Qwen-Template"
+    )
+
+    def print_text(text):
+        print(text)
+        print("[1]", text.count('[1]'))
+        print("[2]", text.count('[2]'))
+        print("[3]", text.count('[3]'))
+        print("[4]", text.count('[4]'))
+        print("[21]", text.count('[21]'))
+        print("[22]", text.count('[22]'))
+        print("[23]", text.count('[23]'))
+        print("[24]", text.count('[24]'))
+        print("[31]", text.count('[31]'))
+        print("[32]", text.count('[32]'))
+        print("[33]", text.count('[33]'))
+        print("[34]", text.count('[34]'))
+
+    text = tokenizer.decode(tokens)
+    print_text(text)
+
+    for batch in dataset:
+        text = tokenizer.decode(batch["tokens"])
+        print_text(text)
+
+
 if __name__ == "__main__":
-    test_mock_gpt_dataset()
+    # test_mock_gpt_dataset()
+    test_gpt_dataset_with_loss_mask()
